b3b062c45dd68dbcea5be83f4d2a45350979214b
lyrapanged <bogart0@yandex.ru>
2023-01-10 23:13:08 +0300

    S14.0001

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -2,9 +2,7 @@ package ru.practicum.shareit.exception;
 
 public class NotFoundException extends RuntimeException {
 
-    private final static String ENTITY_NOT_EXIST = "This '%s' does not exist";
-
     public NotFoundException(String entity) {
-        super(String.format(ENTITY_NOT_EXIST, entity));
+        super(String.format("This '%s' does not exist", entity));
     }
 }
